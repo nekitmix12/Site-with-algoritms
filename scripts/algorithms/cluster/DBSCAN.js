b90f5b7983d59008108d2705c3e3ba25cdf7dcd6
Nikita Verhovskii <nekitverh9@gmail.com>
2024-04-19 09:54:33 +0700

    Small adjustments

--- scripts/algorithms/cluster/DBSCAN.js
+++ scripts/algorithms/cluster/DBSCAN.js
@@ -19,22 +19,22 @@ function dbscan_naive(Points, epsilon, numPointInR, distance) {
         return array;
     }
 
-    function expand_cluster(p, neighbours) {
+    function expand_cluster(tempPoint, neighbours) {
         console.log(clusters[ClusterNumber]===undefined);
         if (clusters[ClusterNumber]===undefined)
             clusters[ClusterNumber] = [];
 
-        clusters[ClusterNumber][clusters[ClusterNumber].length]=p
-        clusteredPoints.add(p)
+        clusters[ClusterNumber][clusters[ClusterNumber].length]=tempPoint
+        clusteredPoints.add(tempPoint)
         while (neighbours.length!==0) {
             let q = neighbours.pop();
             for(let i = 0 ;i<visitedPoints.length;i++)
                 console.log(visitedPoints[i]);
             if (!(visitedPoints.has(q))) {
                 visitedPoints.add(q);
-                let neighbourz = region_query(q);
-                if (neighbourz.length > numPointInR)
-                    neighbours= concatForMatrix(neighbourz, neighbours);
+                let neighbourOfNeighbour = region_query(q);
+                if (neighbourOfNeighbour.length > numPointInR)
+                    neighbours= concatForMatrix(neighbourOfNeighbour, neighbours);
                 console.log(neighbours);
             }
             if (!(clusteredPoints.has(q))) {
@@ -47,11 +47,12 @@ function dbscan_naive(Points, epsilon, numPointInR, distance) {
         }
     }
 
-    for (let i = 0;i<Points.length;i++) {
+    function loopingFunction(i =0){
+        if(i>=Points.length)return clusters;
         let tempPoint = Points[i];
 
         if (visitedPoints.has(tempPoint)) {
-            continue;
+            return  setTimeout(loopingFunction,800,i+1);
         }
 
         visitedPoints.add(tempPoint);
@@ -65,12 +66,14 @@ function dbscan_naive(Points, epsilon, numPointInR, distance) {
 
             expand_cluster(tempPoint, neighbours);
         }
-
+        drawByDictionaryWithRandomColor(clusters,0,0.5,1,0.5)
+        return setTimeout(loopingFunction,800,i+1);
 
     }
 
 
-   drawByDictionaryWithRandomColor(clusters,0,0.5,1,0.5)
+    loopingFunction();
+
 //    drawByDictionaryWithRandomColor(clusters)
     return clusters;
 }
